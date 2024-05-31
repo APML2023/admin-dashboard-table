dd3ff6701c05de4f7110e66e23f9262eb70574c4
itsYashASeeker <yashchauhan.coder@gmail.com>
2024-05-31 17:04:36 +0530

    issues fixed

--- src/component/ReusableTable.jsx
+++ src/component/ReusableTable.jsx
@@ -5,36 +5,60 @@ import ReusableContainer from './ReusableContainer';
 const ReusableTable = ({ data, column, title }) => {
   console.log(data);
 
-  
+
   const totalCount = data.length
   return (
     <div className='w-full'>
-    <ReusableContainer data={data} totalCount = {totalCount}/>
+      <ReusableContainer data={data} totalCount={totalCount} />
       {/* <span>Total Temperature: {totalTemperature}</span> 
       <span>Total Data: {totalCount}</span> */}
-    {
-      data.length && column ?(
-        <div className="table-users ">
-          <div className="header">
-            {title}
-          </div>
-          <table cellSpacing="0">
-            <thead className='table-head`'>
-              
-              <tr>
-                {column.map((item, index) => <TableHeadingColumn data={data} key={index} item={item}/>)}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item, index) => <TableRow key={index} item={item} column={column} />)}
-            </tbody>
-          </table>
-        </div>
+      {
+        data.length && column ? (
+          <div className="table-users ">
+            <div className="header">
+              {title}
+            </div>
+            <table className='w-full overflow-auto' cellSpacing="0">
+              <thead className='table-head'>
 
-      ) :(
-        <SkeletonLoading/>
-      )
-    }
+                <tr>
+                  {column.map((item, index) => <TableHeadingColumn data={data} key={index} item={item} />)}
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => {
+                  return (
+                    <tr>
+                      <td key={index}>{item?.issueDescription}</td>
+                      <td key={index}>{item?.productCategory}</td>
+                      <td key={index}>{item?.productName}</td>
+                      <td key={index}>{item?.dateOfPurchase}</td>
+                      <td key={index}>{item?.timeOfPurchase}</td>
+                      <td key={index}>{item?.createdBy}</td>
+                      <td key={index}>{item?.issueId}</td>
+                      <td key={index}>{item?.type}</td>
+                      {item?.status && item?.status.length ?
+                        <td key={index}>{item?.status[item.status.length - 1]?.status}</td> : <td></td>
+                      }
+                      <td key={index}>{""}</td>
+                      <td key={index}>{item?.createdAt}</td>
+                      <td key={index}>{item?.updatedAt}</td>
+                      <td key={index}>{item?.__v}</td>
+                      <td key={index}><a href={item?.url}>{`${item?.url?.toString().slice(0, 10)}...`}</a></td>
+                      <td key={index}>{item?.issueType}</td>
+                    </tr>
+                  )
+                })}
+
+                {/* {data.map((item, index) => <TableRow key={index} item={item} column={column} />)} */}
+              </tbody>
+            </table>
+          </div>
+
+        ) : (
+          <SkeletonLoading />
+        )
+      }
     </div>
   );
 }
